50831bbe0d69317f43e6e8738544227a0e6d084c
Grant <gflesher0366@myduneland.org>
2021-09-02 07:56:23 -0500

    Initial sharing of project

---
+++ Lion.java
@@ -0,0 +1,19 @@
+
+/**
+ * A program thats about a lion
+ *
+ * @author Grant Flesher
+ * @version 1.0
+ */
+public class Lion
+{
+    private int age;
+
+    /**
+     * Constructor for objects of class Lion
+     */
+    public void roar()
+    {
+        System.out.println("Roar!  Roar!");
+    } // roar() end
+} //Lion end
---
+++ ZooProgram.java
@@ -0,0 +1,14 @@
+
+/**
+ * Make a zoo
+ *
+ * @author Grant Flesher
+ * @version 1.0
+ */
+public class ZooProgram {
+      public static void main (String [] args)
+      {
+           Lion leo = new Lion ();
+        leo.roar();
+    } // main () end
+} // ZooProgram end
